e0b788fc38fd6dafda6abd7d9abcccd786800449
Polina Plesovskih <78843788+tsv-mindal@users.noreply.github.com>
2021-03-20 16:27:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,25 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int g = 0;
+  int s = size - 1;
+  int point = 0;    
+  while (s > point) {
+    
+    int x = (s + point) / 2;
+    if (arr[x] < value)
+    point = x + 1;
+    else
+    s = x;  
+    }
+  if (arr[point] == value) {
+  while (arr[point] == value) {
+    g++;
+    point++;
+    }
+    }
+  if (g)
+  return g;
+  else
   return 0; // если ничего не найдено
 }
